3c9b360730615efcba1355fae70949f482d9f4fa
aleksanderkaczor <aleksanderyt123@gmail.com>
2021-12-13 14:30:40 +0100

    gotowa baza

--- baza.php
+++ baza.php
@@ -8,6 +8,31 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+  <?php
+  $base=mysqli_connect('localhost','root','','pizzeria');
+  $request="select id, nazwa_pizzy, cena_32, cena_42, skladniki from menu"; 
+  $result=mysqli_query($base, $request);
+  while($wiersz=mysqli_fetch_array($result)){
+      echo"<li>". 
+      $wiersz['id']. 
+      "</li>";
+      echo"<li>". 
+      $wiersz['nazwa_pizzy']. 
+      "</li>";
+      echo"<li>". 
+      $wiersz['cena_32']. 
+      "</li>";
+      echo"<li>". 
+      $wiersz['cena_42']. 
+      "</li>";
+      echo"<li>". 
+      $wiersz['skladniki']. 
+      "</li>";
+  }
+  echo"</ol>";
+  mysqli_close($base);
+  ?>
     
 <script src="script.js"></script>
 </body>
